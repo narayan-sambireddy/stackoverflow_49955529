353e6776d5452c5c806d1eae57b59ed8e924470a
Narayan Sambi Reddy <narayan-sambireddy@outlook.com>
2018-04-25 09:45:02 +0530

    Update Demo4BrettApp.java

--- src/main/com/assignment/restapi/Demo4BrettApp.java
+++ src/main/com/assignment/restapi/Demo4BrettApp.java
@@ -35,13 +35,13 @@ public class Demo4BrettApp {
 	public ProductResponse updateProduct(
 		@PathVariable("productId") Long productId,
 		@Validated @RequestBody ProductForm productForm) throws Exception { 
-	// @Validated will take care of incorporating validation rules on incoming ProductForm JSON request body
-	Optional<Product> foundProductOpt = productRepository.findById(productId);
-	Product foundProduct = foundProductOpt.orElseThrow(() -> new EntityNotFoundException("productId" + productId + "not found."));
-	org.apache.commons.beanutils.BeanUtils.copyProperties(foundProduct, productForm);
-	productRepository.save(foundProduct);
+		// @Validated will take care of incorporating validation rules on incoming ProductForm JSON request body
+		Optional<Product> foundProductOpt = productRepository.findById(productId);
+		Product foundProduct = foundProductOpt.orElseThrow(() -> new EntityNotFoundException("productId" + productId + "not found."));
+		org.apache.commons.beanutils.BeanUtils.copyProperties(foundProduct, productForm);
+		productRepository.save(foundProduct);
 
-	return new ProductResponse(null, "product updated");
+		return new ProductResponse(null, "product updated");
 	}
 	
 	@ExceptionHandler(Exception.class)
